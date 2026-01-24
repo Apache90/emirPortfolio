8a49b86cf1ff90062f4721db44eeda0abef926e4
Emir Segovia <95300946+Apache90@users.noreply.github.com>
2026-01-24 14:21:40 -0300

    se fixeo la permanencia del menu lateral

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet"
 import { usePathname } from "next/navigation";
 import Link from "next/link";
@@ -25,24 +26,31 @@ const links = [
 
 const MobileNav = () => {
     const pathname = usePathname();
+    const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger className="flex justify-center items-center">
             <CiMenuFries className="text-[32px] text-accent"/>
         </SheetTrigger>
         <SheetContent className="flex flex-col">
             {/* logo */}
             <div className="mt-32 mb-40 text-center text-2xl">
-                <Link href="/">
+                <Link href="/" onClick={() => setIsOpen(false)}>
                 <h1 className="text-4xl font-semibold text-black">Emir<span className="text-accent">.</span></h1>
                 </Link>
             </div>
             {/* nav */}
             <nav className= "flex flex-col justify-center items-center gap-8 text-black/70">   
                 {links.map((link, index)=> {
-                return <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>
-                {link.name}
-            </Link>
+                return <Link 
+                    href={link.path} 
+                    key={index} 
+                    onClick={() => setIsOpen(false)}
+                    className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}
+                >
+                    {link.name}
+                </Link>
         })}
     </nav>
 
